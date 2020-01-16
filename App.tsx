49a1e93af56c6fe72e2d5cd676f22634543757f6
Olie Chan <olie.chan@infinityworks.com>
2020-01-16 09:33:19 +0000

    Minimal navigation app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Button } from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+function HomeScreen({ navigation }: any) {
+	return (
+		<View style={styles.container}>
+			<Text>Home Screen</Text>
+			<Button title="Go to the details screen"
+				type="outline"
+				onPress={() => navigation.navigate('Details')}
+			/>
+		</View>
+	);
 }
+function DetailsScreen({ navigation }: any) {
+	return (
+		<View style={styles.container}>
+			<Text>Details Screen</Text>
+		</View>
+	);
+}
+const AppNavigator = createStackNavigator({
+	Home: HomeScreen,
+	Details: DetailsScreen,
+}, {
+	initialRouteName: 'Home'
+});
+
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
