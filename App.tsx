eec56562c646d330a666fc662e9352bd3ea5ab93
Olie Chan <olie.chan@infinityworks.com>
2020-01-16 09:59:25 +0000

    Pass params into the navigator

--- App.tsx
+++ App.tsx
@@ -10,7 +10,10 @@ function HomeScreen({ navigation }: any) {
 			<Text>Home Screen</Text>
 			<Button title="Go to the details screen"
 				type="outline"
-				onPress={() => navigation.navigate('Details')}
+				onPress={() => navigation.navigate('Details', {
+					itemId: 123,
+					otherParam: 'anything that you want here',
+				})}
 			/>
 		</View>
 	);
@@ -19,6 +22,15 @@ function DetailsScreen({ navigation }: any) {
 	return (
 		<View style={styles.container}>
 			<Text>Details Screen</Text>
+			<Text>
+				itemId: {JSON.stringify(navigation.getParam('itemId', null))}
+			</Text>
+			<Text>
+				otherParam: {JSON.stringify(navigation.getParam('otherParam', null))}
+			</Text>
+			<Text>
+				somer otherParam: {JSON.stringify(navigation.getParam('doesNotExist', 'This param does not exist'))}
+			</Text>
 			<Button title="Go to the details screen again?"
 				type="outline"
 				onPress={() => navigation.push('Details')}
